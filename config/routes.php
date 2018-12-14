8a2990de27af367f733c23dc56397cab492e88f3
mirko-pagliai <mirko.pagliai@gmail.com>
2018-12-14 14:19:30 +0100

    fixed

--- config/routes.php
+++ config/routes.php
@@ -15,7 +15,7 @@ use Cake\Routing\Router;
 
 Router::plugin(ASSETS, ['path' => '/assets'], function (RouteBuilder $routes) {
     $routes->connect('/:filename', ['controller' => 'Assets', 'action' => 'asset'], [
-        'filename' => '[a-z0-9]+\.(css|js)',
+        'filename' => '[\d\w]+\.(css|js)',
         'pass' => ['filename'],
     ]);
 });
