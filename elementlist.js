10d35410d9ae0aa78efc9c1c80c2080c7d314227
mirko-pagliai <mirko.pagliai@gmail.com>
2019-11-14 14:25:37 +0100

    updated

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Assets\\Http\\Exception\\AssetNotFoundException"],["c","Assets\\Plugin"],["c","Assets\\Routing\\Middleware\\AssetMiddleware"],["c","Assets\\TestSuite\\TestCase"],["c","Assets\\Utility\\AssetsCreator"],["c","Assets\\View\\Helper\\AssetHelper"]];
